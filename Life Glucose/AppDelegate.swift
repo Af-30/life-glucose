6e57d9e62b55638bc7c9f8814bcf8c72d5202835
AF-30 <grandahmad40@gmail.com>
2021-12-17 07:57:47 +0300

    add firebase package

--- Life Glucose/AppDelegate.swift
+++ Life Glucose/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+//        let db = Firestore.firestore()
+//        db.collection("doctors").addDocument(data: ["name": "azef", "id": 123]) { error in
+//            if let error = error {
+//                print(error.localizedDescription)
+//                return
+//            }
+//            print("ADDED")
+//        }
+        
         return true
     }
 
